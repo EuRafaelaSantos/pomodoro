c7e1d5d130b659bc74932821b7f6b1fd3eb689a1
EuRafaelaSantos <rafaelasantos.informatica@gmail.com>
2024-10-15 18:40:25 -0400

    ajustando o tempo do pomodoro

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -11,15 +11,15 @@ abstract class _PomodoroStore with Store {
   @observable
   bool iniciado = false;
   @observable
-  int minutos = 1;
+  int minutos = 25;
 
   @observable
   int segundos = 0;
   @observable
-  int tempoTrabalho = 1;
+  int tempoTrabalho = 25;
 
   @observable
-  int tempoDescanso = 1;
+  int tempoDescanso = 5;
 
   @observable
   TipoIntervalo tipoIntervalo = TipoIntervalo.TRABALHO;
